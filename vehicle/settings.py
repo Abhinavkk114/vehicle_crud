8a5bd9b42bd31c829d9b8cdb54c9513e221782a7
Abhinavkk114 <abhinavkk114@gmail.com>
2023-07-17 11:20:10 +0530

    crud

--- vehicle/settings.py
+++ vehicle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4qbio@wm38h($q^d7bo964wfgd63r-9^i%r(3a@@lx@l2ek#((
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
